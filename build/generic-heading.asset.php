2407b95bbb5cadc627284794ac2a3a4fab165fc2
Miguel Caballero <mc.caballero95@gmail.com>
2025-03-20 10:11:19 -0400

    fixing build command

---
+++ build/generic-heading.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'ee6a32100cc6657e9557');
---
+++ build/slideshow.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'b82b684801ef286d07fd');
